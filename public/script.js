adaad963f3bcab5473e1863f05dd99a55aac6642
Gabriel Civinski <gabrielcivinski@gmail.com>
2019-06-14 16:37:07 -0300

    fix deploy route

--- public/script.js
+++ public/script.js
@@ -24,7 +24,7 @@ const InsertTemplateInTheChat = (template) => {
 const getWatsonMessageAndInsertTemplate = async (text = '') => {
   document.getElementById("animation").style.display = "block";
   //const uri = 'http://localhost:3000/conversation/';
-  const uri = 'http://chatbot-cheerful-antelope.mybluemix.net/conversation/'
+  const uri = 'https://chatbot-cheerful-antelope.mybluemix.net/conversation/'
 
   const response = await (await fetch(uri, {
     method: 'POST',
